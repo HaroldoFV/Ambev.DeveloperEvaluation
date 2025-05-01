332a04660772ceefd24c727ac2cb6d72f8b47b37
Haroldo Vinente <haroldovinente@gmail.com>
2025-05-01 11:10:27 -0300

    feat: add domain event publishing mechanism with tests
    Introduced `DomainEventPublisher` to handle domain event publishing and `IDomainEventHandler` interface to manage event handling logic. Included unit tests to verify proper event handling and ignored events when no subscribers exist. Added Moq package for mocking dependencies in tests.

---
+++ tests/Ambev.DeveloperEvaluation.Unit/Application/DomainEventFake.cs
@@ -0,0 +1,11 @@
+﻿using Ambev.DeveloperEvaluation.Domain.SeedWork;
+
+namespace Ambev.DeveloperEvaluation.Unit.Application;
+
+public class DomainEventToBeHandledFake : DomainEvent
+{
+}
+
+public class DomainEventToNotBeHandledFake : DomainEvent
+{
+}
