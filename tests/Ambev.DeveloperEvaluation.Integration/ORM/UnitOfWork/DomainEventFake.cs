cea63c5a7c671a3300b8fa56760516fa18d2bfd8
Haroldo Vinente <haroldovinente@gmail.com>
2025-05-01 17:02:43 -0300

    feat: add integration tests for UnitOfWork and event handling
    Introduce integration tests for UnitOfWork, covering commit and rollback functionality. Implement DomainEventFake for testing purposes and update SaleConfiguration to ignore domain events. Refactor UnitOfWork to properly handle and publish domain events during commit operations.

---
+++ tests/Ambev.DeveloperEvaluation.Integration/ORM/UnitOfWork/DomainEventFake.cs
@@ -0,0 +1,7 @@
+﻿using Ambev.DeveloperEvaluation.Domain.SeedWork;
+
+namespace Ambev.DeveloperEvaluation.Integration.ORM.UnitOfWork;
+
+public class DomainEventFake : DomainEvent
+{
+}
---
+++ tests/Ambev.DeveloperEvaluation.Integration/ORM/UnitOfWork/UnitOfWorkTest.cs
@@ -0,0 +1,72 @@
+﻿using Ambev.DeveloperEvaluation.Application;
+using Ambev.DeveloperEvaluation.Domain.SeedWork;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace Ambev.DeveloperEvaluation.Integration.ORM.UnitOfWork;
+
+[Collection(nameof(UnitOfWorkTestFixture))]
+public class UnitOfWorkTest
+{
+    private readonly UnitOfWorkTestFixture _fixture;
+
+    public UnitOfWorkTest(UnitOfWorkTestFixture fixture)
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(CommitAsync))]
+    [Trait("Integration/Infra.Data", "UnitOfWork - Persistence")]
+    public async Task CommitAsync()
+    {
+        var dbContext = _fixture.CreateDbContext();
+        var exampleSalesList = _fixture.GetExampleSalesList();
+        var saleWithEvent = exampleSalesList.First();
+        var @event = new DomainEventFake();
+        saleWithEvent.RaiseEvent(@event);
+        var eventHandlerMock = new Mock<IDomainEventHandler<DomainEventFake>>();
+        await dbContext.AddRangeAsync(exampleSalesList);
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddLogging();
+        serviceCollection.AddSingleton(eventHandlerMock.Object);
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+        var eventPublisher = new DomainEventPublisher(serviceProvider);
+        var unitOfWork = new DeveloperEvaluation.ORM.UnitOfWork(dbContext,
+            eventPublisher,
+            serviceProvider.GetRequiredService<ILogger<DeveloperEvaluation.ORM.UnitOfWork>>());
+
+        await unitOfWork.CommitAsync(CancellationToken.None);
+
+        var assertDbContext = _fixture.CreateDbContext(true);
+        var savedSales = assertDbContext.Sales
+            .AsNoTracking().ToList();
+        savedSales.Should()
+            .HaveCount(exampleSalesList.Count);
+        eventHandlerMock.Verify(x =>
+                x.HandleAsync(@event, It.IsAny<CancellationToken>()),
+            Times.Once);
+        saleWithEvent.Events.Should().BeEmpty();
+    }
+
+
+    [Fact(DisplayName = nameof(Rollback))]
+    [Trait("Integration/Infra.Data", "UnitOfWork - Persistence")]
+    public async Task Rollback()
+    {
+        var dbContext = _fixture.CreateDbContext();
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddLogging();
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+        var eventPublisher = new DomainEventPublisher(serviceProvider);
+        var unitOfWork = new DeveloperEvaluation.ORM.UnitOfWork(dbContext,
+            eventPublisher,
+            serviceProvider.GetRequiredService<ILogger<DeveloperEvaluation.ORM.UnitOfWork>>());
+
+        var task = async ()
+            => await unitOfWork.RollbackAsync(CancellationToken.None);
+
+        await task.Should().NotThrowAsync();
+    }
+}
